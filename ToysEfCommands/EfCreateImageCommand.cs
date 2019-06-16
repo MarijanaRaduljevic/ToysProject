74d0406c692bab87631fa18bf365f336e2275759
MarijanaRaduljevic <51883017+MarijanaRaduljevic@users.noreply.github.com>
2019-06-16 23:47:06 +0200

    Add files via upload

---
+++ ToysEfCommands/EfCreateImageCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToysApplication.Commands;
+using ToysApplication.DTO;
+using ToysDomain;
+using ToysEfDataAccess;
+
+namespace ToysEfCommands
+{
+    public class EfCreateImageCommand : ICreateImageCommand
+    {
+        private readonly ToysContext _context;
+
+        public EfCreateImageCommand(ToysContext context)
+        {
+            _context = context;
+        }
+
+        public void Execute(CreateImageDto request)
+        {
+            _context.Images.Add(new Image
+            {
+                
+                CreatedAt = DateTime.Now,
+                Src = request.Src,
+                Alt = request.Alt,
+                Title = request.Title
+            });
+
+            _context.SaveChanges();
+        }
+    }
+    }
+
