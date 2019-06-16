dfbfb07da4ea763e9ce77db6a08596cb7938596c
MarijanaRaduljevic <51883017+MarijanaRaduljevic@users.noreply.github.com>
2019-06-16 23:44:38 +0200

    Add files via upload

--- ToysApplication/DTO/CreateImageDto.cs
+++ ToysApplication/DTO/CreateImageDto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -23,5 +25,8 @@ namespace ToysApplication.DTO
 
         [Required]
         public string Title { get; set; }
+
+        public IFormFile Image { get; set; }
+
     }
 }
